fd31e6eab0a4fa6febd18916a2e87b3da6e187c0
Andy.Zhang <andycnzh@outlook.com>
2013-03-05 23:48:31 +0800

    using the  generics method to implement the bubble sort.

--- PCLAndyCnZh/BubbleSort.cs
+++ PCLAndyCnZh/BubbleSort.cs
@@ -37,45 +37,17 @@ namespace PCLAndyCnZh
                 return unsortArray;
             }
 
-            public double[] ExecuteBubbleSort(double[] unsortArray)
-            {
-                double temp = 0;
-
-                for (int i = 0; i < unsortArray.Length - 1; i++)
-                {
-                    bool swap = false;
-
-                    for (int j = unsortArray.Length - 1; j > i; j--)
-                    {
-                        if (unsortArray[j] < unsortArray[j - 1])
-                        {
-                            temp = unsortArray[j];
-                            unsortArray[j] = unsortArray[j - 1];
-                            unsortArray[j - 1] = temp;
-                            swap = true;
-                        }
-                    }
-
-                    if (!swap)
-                    {
-                        break;
-                    }
-                }
-
-                return unsortArray;
-            }
-
-            public List<T> ExecuteBubbleSort<T>(List<T> sortList)
+            public static T[] ExecuteBubbleSort<T>(T[] sortList) where T : IComparable<T>
             {
                 T temp;
 
-                for (int i = 0; i < sortList.Count - 1; i++)
+                for (int i = 0; i < sortList.Length - 1; i++)
                 {
                     bool swap = false;
 
-                    for (int j = sortList.Count - 1; j > i; j--)
+                    for (int j = sortList.Length - 1; j > i; j--)
                     {
-                        if (Comparison(sortList[j], sortList[j - 1]))
+                        if (sortList[j].CompareTo(sortList[j - 1]) < 0)
                         {
                             temp = sortList[j];
                             sortList[j] = sortList[j - 1];
@@ -92,6 +64,35 @@ namespace PCLAndyCnZh
 
                 return sortList;
             }
+
+            public static List<T> ExecuteBubbleSort<T>(List<T> sortList) where T : IComparable<T>
+            {
+                T temp;
+
+                for (int i = 0; i < sortList.Count - 1; i++)
+                {
+                    bool swap = false;
+
+                    for (int j = sortList.Count - 1; j > i; j--)
+                    {
+                        if (sortList[j].CompareTo(sortList[j - 1]) < 0)
+                        {
+                            temp = sortList[j];
+                            sortList[j] = sortList[j - 1];
+                            sortList[j - 1] = temp;
+                            swap = true;
+                        }
+                    }
+
+                    if (!swap)
+                    {
+                        break;
+                    }
+                }
+
+                return sortList;
+            }
+
         }
     }
 }
--- PCLAndyCnZhTestApplication/BubbleSortTest.cs
+++ PCLAndyCnZhTestApplication/BubbleSortTest.cs
@@ -15,7 +15,8 @@ namespace PCLAndyCnZhTestApplication
             int[] intArr = new int[9] { 2, 1, 4, 3, 6, 5, 8, 7, 9 };
             int[] sortIntArr = new int[9];
 
-            sortIntArr = bs.ExecuteBubbleSort(intArr);
+            //sortIntArr = bs.ExecuteBubbleSort(intArr);
+            sortIntArr = BubbleSort.ExecuteBubbleSort<int>(intArr);
 
             for (int i = 0; i < sortIntArr.Length; i++)
             {
@@ -26,14 +27,27 @@ namespace PCLAndyCnZhTestApplication
 
             double[] doubleArr = new double[9] { 2.2, 1.1, 4.4, 3.3, 6.6, 5.5, 8.8, 7.7, 9.9 };
             double[] sortDoubleArr = new double[9];
-
-
-            sortDoubleArr = bs.ExecuteBubbleSort(doubleArr);
+            
+            //sortDoubleArr = bs.ExecuteBubbleSort(doubleArr);
+            sortDoubleArr = BubbleSort.ExecuteBubbleSort<double>(doubleArr);
 
             for (int i = 0; i < sortDoubleArr.Length; i++)
             {
                 Console.Write(sortDoubleArr[i] + ",");
             }
+
+            Console.WriteLine();
+
+            string[] strArr = new string[9] { "bbb", "aaa", "ddd", "ccc", "fff", "eee", "hhh", "ggg", "iii" };
+            strArr = BubbleSort.ExecuteBubbleSort<string>(strArr);
+
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                Console.Write(strArr[i] + ",");
+            }
+
+
+
         }
     }
 }
